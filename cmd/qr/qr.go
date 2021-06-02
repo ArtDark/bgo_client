c9a1157595b596b8014da3934f46fcc27a0f7540
ArtDark <balusoff@gmail.com>
2021-06-02 15:09:47 +0300

    add func QrCreator add QR request structure

--- cmd/qr/qr.go
+++ cmd/qr/qr.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"github.com/ArtDark/bgo_client/pkg/qr"
+	"log"
+)
+
 func main() {
+
+	//TODO: написать тайм-аут
+
+	err := qr.QrCreator()
+	if err != nil {
+		log.Println(err)
+	}
+
+	log.Println("File created...")
 
 }
--- pkg/qr/qr.go
+++ pkg/qr/qr.go
@@ -1,1 +1,50 @@
 package qr
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+type QrApi struct {
+	url     string
+	version string
+	method  string
+	data    string
+} //TODO: Написать структуру запроса
+
+func QrCreator() error { //TODO: превратить в сервис
+	reqURL := "http://api.qrserver.com/v1/create-qr-code/?data=Ты%20пидор!&size=100x100"
+
+	resp, err := http.Get(reqURL)
+	defer resp.Body.Close()
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	file, err := os.Create("image.png")
+	if err != nil {
+		log.Println("Cannot open file", err)
+	}
+	defer func(c io.Closer) {
+		if cerr := c.Close(); cerr != nil {
+			log.Println("Cannot close file", cerr)
+		}
+	}(file)
+
+	_, err = file.Write(body)
+	if err != nil {
+		log.Println("Cannot open file", err)
+	}
+
+	return nil
+
+}
